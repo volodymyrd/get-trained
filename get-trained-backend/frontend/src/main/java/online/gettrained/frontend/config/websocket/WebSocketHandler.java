582a7779f3ec978a6a78fafe6625d5f8c6c872a6
volodymyrd <volodymyrdotsenko@gmail.com>
2019-03-08 23:07:38 +0100

    add logger to websocket handler.

--- get-trained-backend/frontend/src/main/java/online/gettrained/frontend/config/websocket/WebSocketHandler.java
+++ get-trained-backend/frontend/src/main/java/online/gettrained/frontend/config/websocket/WebSocketHandler.java
@@ -6,6 +6,8 @@ import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 import java.util.concurrent.ConcurrentHashMap;
 import online.gettrained.backend.services.user.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -18,6 +20,8 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 @Component
 public class WebSocketHandler extends TextWebSocketHandler {
 
+  private static final Logger LOG = LoggerFactory.getLogger(WebSocketHandler.class);
+
   private final ConcurrentHashMap<String, String> sessions = new ConcurrentHashMap<>();
   private final Multimap<String, WebSocketSession> userSessions =
       synchronizedMultimap(HashMultimap.create());
@@ -29,14 +33,11 @@ public class WebSocketHandler extends TextWebSocketHandler {
   }
 
   @Override
-  protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-    System.out.println(session.getPrincipal() + ": " + message);
-    session.sendMessage(new TextMessage("Hi Hi!!"));
+  protected void handleTextMessage(WebSocketSession session, TextMessage message) {
   }
 
   @Override
-  public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-    System.out.println(session);
+  public void afterConnectionEstablished(WebSocketSession session) {
     if (session.getPrincipal() != null) {
       userService.findOneByEmail(session.getPrincipal().getName()).ifPresent(
           user -> {
@@ -47,15 +48,14 @@ public class WebSocketHandler extends TextWebSocketHandler {
   }
 
   @Override
-  public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-    System.out.println(session);
+  public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
     userSessions.remove(sessions.get(session.getId()), session);
     sessions.remove(session.getId());
   }
 
   @Override
-  public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
-    System.out.println(session);
-    System.out.println(exception);
+  public void handleTransportError(WebSocketSession session, Throwable exception) {
+    LOG.error("Websocket transport error for user:", session.getPrincipal());
+    LOG.error("Websocket transport error", exception);
   }
 }
