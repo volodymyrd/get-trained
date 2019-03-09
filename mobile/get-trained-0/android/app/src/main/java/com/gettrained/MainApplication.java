0fb6226fd97ae64aed5e6c99a080a223398191df
volodymyrd <volodymyrdotsenko@gmail.com>
2019-03-09 15:54:50 +0100

    add react-native-vector-icons lib.

--- mobile/get-trained-0/android/app/src/main/java/com/gettrained/MainApplication.java
+++ mobile/get-trained-0/android/app/src/main/java/com/gettrained/MainApplication.java
@@ -2,11 +2,11 @@ package com.gettrained;
 
 import android.app.Application;
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import java.util.Arrays;
 import java.util.List;
