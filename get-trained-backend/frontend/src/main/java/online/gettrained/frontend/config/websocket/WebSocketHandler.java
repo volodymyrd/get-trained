7cfeb4943105a3b11024ebaeee423f2140d30a14
volodymyrd <volodymyrdotsenko@gmail.com>
2019-03-12 00:28:00 +0100

    add getting user info from websocket session.

--- get-trained-backend/frontend/src/main/java/online/gettrained/frontend/config/websocket/WebSocketHandler.java
+++ get-trained-backend/frontend/src/main/java/online/gettrained/frontend/config/websocket/WebSocketHandler.java
@@ -4,7 +4,11 @@ import static com.google.common.collect.Multimaps.synchronizedMultimap;
 
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
+import java.io.IOException;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import online.gettrained.backend.domain.user.CurrentUser;
+import online.gettrained.backend.domain.user.User;
 import online.gettrained.backend.services.user.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,7 +26,7 @@ public class WebSocketHandler extends TextWebSocketHandler {
 
   private static final Logger LOG = LoggerFactory.getLogger(WebSocketHandler.class);
 
-  private final ConcurrentHashMap<String, String> sessions = new ConcurrentHashMap<>();
+  private final ConcurrentHashMap<WebSocketSession, String> sessions = new ConcurrentHashMap<>();
   private final Multimap<String, WebSocketSession> userSessions =
       synchronizedMultimap(HashMultimap.create());
 
@@ -34,28 +38,70 @@ public class WebSocketHandler extends TextWebSocketHandler {
 
   @Override
   protected void handleTextMessage(WebSocketSession session, TextMessage message) {
+    sessions.forEach((k, v) -> LOG.info("Session for user: {}", k.getPrincipal()));
+
+    LOG.info("Got a message {}", message.getPayload());
+
+    Optional<User> optionalUser = getUser(session);
+
+    if (optionalUser.isPresent()) {
+      User user = optionalUser.get();
+      LOG.info("Got message from a user: {}", user.getEmail());
+      sessions.entrySet().stream()
+          .filter(e -> !e.getValue().equals(user.getEmail()))
+          .forEach(e -> {
+            try {
+              e.getKey().sendMessage(new TextMessage(message.getPayload()));
+            } catch (IOException ex) {
+              ex.printStackTrace();
+            }
+          });
+    } else {
+      LOG.warn("Got a message from undefined user");
+    }
   }
 
   @Override
   public void afterConnectionEstablished(WebSocketSession session) {
-    if (session.getPrincipal() != null) {
-      userService.findOneByEmail(session.getPrincipal().getName()).ifPresent(
-          user -> {
-            sessions.put(session.getId(), user.getEmail());
-            userSessions.put(user.getEmail(), session);
-          });
+    LOG.info("Established connection...");
+    Optional<User> optionalUser = getUser(session);
+
+    if (optionalUser.isPresent()) {
+      User user = optionalUser.get();
+      LOG.info("Established a session for user: {}", user.getEmail());
+      sessions.put(session, user.getEmail());
+      userSessions.put(user.getEmail(), session);
+    } else {
+      LOG.info("Established a session for undefined user, try to close it...");
+      try {
+        session.close();
+      } catch (IOException e) {
+        LOG.error("Error close a session for undefined user: ", e);
+      }
     }
   }
 
   @Override
   public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
-    userSessions.remove(sessions.get(session.getId()), session);
-    sessions.remove(session.getId());
+    LOG.info("Close a session for user: ", session.getPrincipal());
+    userSessions.remove(sessions.get(session), session);
+    sessions.remove(session);
   }
 
   @Override
   public void handleTransportError(WebSocketSession session, Throwable exception) {
     LOG.error("Websocket transport error for user:", session.getPrincipal());
     LOG.error("Websocket transport error", exception);
+  }
+
+  private Optional<User> getUser(WebSocketSession session) {
+    if (session.getPrincipal() != null) {
+      if (session.getPrincipal() instanceof CurrentUser) {
+        return Optional.of(((CurrentUser) session.getPrincipal()).getUser());
+      } else if (session.getPrincipal().getName() != null) {
+        return userService.findOneByEmail(session.getPrincipal().getName());
+      }
+    }
+    return Optional.empty();
   }
 }
